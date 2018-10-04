f48d66451aac72ca539abf67f44954de058059ca
Zach Ashworth <zashworth2@gmail.com>
2018-10-03 21:01:43 -0500

    finished assignments unordered

---
+++ for_loop_basic1.py
@@ -0,0 +1,36 @@
+# for count in range(0,151):
+#     print(count)
+
+
+# for count in range(0,1000000):
+#     if count%5 == 0:  
+#         print(count)
+#     else:
+#         continue
+
+# for count in range(1,101):
+#     if count%5 == 0:
+#         print('coding')
+#     if count%10 == 0:
+#         print('dojo')
+
+# sum=0
+# for count in range(0,500000):
+#     if count%2 != 0:
+#         sum=sum+count
+# print(sum)
+
+# x=2018
+# while x > 2:
+#     x=x-4
+#     print(x)
+
+# def flex(low,high,mult):
+#     for count in range(low,high+1):
+#         if count%mult==0:
+#             print(count)
+# flex(2,9,3)
+
+# 3,5,1,2
+#error
+# 0,1,2,3
